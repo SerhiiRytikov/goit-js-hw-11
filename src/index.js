d8cfd935470a4714b4d5f5120e2be46112c7cd70
SerhiiRytikov <serg_ritikov@ukr.net>
2023-05-31 13:20:11 +0300

    1

--- src/index.js
+++ src/index.js
@@ -22,58 +22,90 @@ window.addEventListener('scroll', () => {
   }
 });
 
-function onSearch(event) {
+async function onSearch(event) {
   event.preventDefault();
 
-  searchQuery = event.currentTarget.searchQuery.value;
+  searchQuery = event.currentTarget.searchQuery.value.trim(); // Видаляємо зайві пробіли з початку і кінця рядка
+  if (!searchQuery) {
+    // Перевірка на пустий рядок
+    Notify.failure('Please enter a search query.');
+    return;
+  }
+
   resetPage();
 
-  axios
-    .get(
+  try {
+    const response = await axios.get(
       `https://pixabay.com/api/?key=36616825-90ae0210fb817fe2b1f6bb41c&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`
-    )
-    .then(res => {
-      if (res.data.hits.length > 0) {
-        Notify.success(`Hooray! We found ${res.data.totalHits} images.`);
-      }
-      if (res.data.hits.length === 0) {
-        Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        return;
-      }
-      createMarkup(res.data.hits);
-      lightbox.refresh();
-      page += 1;
-    })
-    .catch(error => error);
+    );
+
+    const { data } = response;
+
+    if (data.hits.length > 0) {
+      Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    } else {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+
+    createMarkup(data.hits);
+    lightbox.refresh();
+    page += 1;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function onLoadMore() {
-  axios
-    .get(
-      `https://pixabay.com/api/?key=36616825-90ae0210fb817fe2b1f6bb41c&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
-    )
-    .then(res => {
-      if (res.data.hits.length === 0) {
-        Notify.info(
-          "We're sorry, but you've reached the end of search results."
-        );
-        return;
-      }
-      Notify.success(`Hooray! We found ${res.data.totalHits} images.`);
-      createMarkup(res.data.hits);
-      page += 1;
-      const { height: cardHeight } = document
-        .querySelector('.gallery')
-        .firstElementChild.getBoundingClientRect();
+let isLoading = false;
 
-      window.scrollBy({
-        top: cardHeight * 2 + 150,
-        behavior: 'smooth',
-      });
-    })
-    .catch(error => error);
+async function onLoadMore() {
+  if (isLoading || !searchQuery) {
+    return;
+  }
+
+  isLoading = true;
+
+  try {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=36616825-90ae0210fb817fe2b1f6bb41c&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+    );
+
+    const { data } = response;
+
+    if (data.hits.length === 0) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+      return;
+    }
+
+    Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    createMarkup(data.hits);
+    lightbox.refresh();
+
+    const { totalHits, hits } = data;
+    const remainingHits = totalHits - page * 40;
+
+    if (remainingHits <= 0) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+      return;
+    }
+
+    page += 1;
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2 + 150,
+      behavior: 'smooth',
+    });
+  } catch (error) {
+    console.error(error);
+  } finally {
+    isLoading = false;
+  }
 }
 
 function resetPage() {
